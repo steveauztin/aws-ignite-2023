bba543b7e3ae73ea7a68f539e60914b493b524a7
sfernandez <sfernandez@niubiz.com.pe>
2023-06-14 00:57:41 -0500

    change in commands

--- rds/webserver/user-data.sh
+++ rds/webserver/user-data.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 dnf install -y httpd php php-mysqli mariadb105
+dnf install -y git
 
 case $(ps -p 1 -o comm | tail -1) in 
 systemd) systemctl enable --now httpd ;;
@@ -9,8 +10,13 @@ esac
 
 if [ ! -f /var/www/html/bootcamp-app.tar.gz ]; then
 cd /var/www/html
-wget https://s3.amazonaws.com/immersionday-labs/bootcamp-app.tar
-tar xvf bootcamp-app.tar
+# wget https://s3.amazonaws.com/immersionday-labs/bootcamp-app.tar
+# tar xvf bootcamp-app.tar
+git clone https://github.com/steveauztin/aws-ignite-2023.git
+cd aws-ignite-2023
+mv rds/app/* ../
+cd ..
+rm -fr aws-ignite-2023
 chown apache:root /var/www/html/rds.conf.php
 fi
 dnf update -y
